9d19b903a94e32e239c86187be9738333dd8c3fb
sagarrathod7568 <sagarrathod050@gmail.com>
2026-06-12 23:25:11 +0530

    setup generateToken and activity logs with task and user

---
+++ Backend/models/Task.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: String,
+
+    status: {
+      type: String,
+      enum: ["Pending", "In Progress", "Completed"],
+      default: "Pending",
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
